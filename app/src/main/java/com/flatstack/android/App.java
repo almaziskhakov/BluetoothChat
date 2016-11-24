44b61125fa7f5175b5bfb311b1b9a9783d07f929
AlmazIskhakovFlatstack <almaz.iskhakov@flatstack.com>
2016-11-24 12:37:54 +0300

    Create bluetooth interaction

--- app/src/main/java/com/flatstack/android/App.java
+++ app/src/main/java/com/flatstack/android/App.java
@@ -3,17 +3,33 @@ package com.flatstack.android;
 import android.app.Application;
 
 import com.flatstack.android.utils.TimberCrashReportingTree;
+import com.flatstack.android.utils.di.AppComponent;
+import com.flatstack.android.utils.di.AppModule;
+import com.flatstack.android.utils.di.DaggerAppComponent;
 
 import timber.log.Timber;
 
 public class App extends Application {
 
+    private AppComponent appComponent;
+
     @Override
     public void onCreate() {
         super.onCreate();
+
+        // Dagger%COMPONENT_NAME%
+        appComponent = DaggerAppComponent.builder()
+//             list of modules that are part of this component need to be created here too
+                .appModule(new AppModule(this)) // This also corresponds to the name of your module: %component_name%Module
+                .build();
+
         Timber.plant(BuildConfig.DEBUG
                 ? new Timber.DebugTree()
                 : new TimberCrashReportingTree());
+    }
+
+    public AppComponent getAppComponent() {
+        return appComponent;
     }
 
 }
